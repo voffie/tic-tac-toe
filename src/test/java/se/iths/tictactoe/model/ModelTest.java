6aafc8a2353db2dfe1cdc3888b459de125511780
voffie <viktorsoderborg@gmail.com>
2024-10-23 14:20:01 +0200

    Add coverage tests

--- src/test/java/se/iths/tictactoe/model/ModelTest.java
+++ src/test/java/se/iths/tictactoe/model/ModelTest.java
@@ -33,83 +33,83 @@ class ModelTest {
     @Test
     void canUpdateBoard() {
         String[][] board = {{"X", "", ""}, {"", "", ""}, {"", "", ""}};
-        model.setToken(0,0); // X
+        model.setToken(0, 0); // X
         assertArrayEquals(board, model.getBoard());
     }
 
     @Test
     void canNotAddTokenOnOccupiedCell() {
         String[][] board = {{"X", "", ""}, {"", "", ""}, {"", "", ""}};
-        model.setToken(0,0); // X
-        model.setToken(0,0); // O
+        model.setToken(0, 0); // X
+        model.setToken(0, 0); // O
 
         assertArrayEquals(board, model.getBoard());
     }
 
     @Test
     void boardShouldBeWonVertically() {
-        model.setToken(0,0); // X
-        model.setToken(0,1); // O
-        model.setToken(1,0); // X
-        model.setToken(0,2); // O
-        model.setToken(2,0); // X
+        model.setToken(0, 0); // X
+        model.setToken(0, 1); // O
+        model.setToken(1, 0); // X
+        model.setToken(0, 2); // O
+        model.setToken(2, 0); // X
 
         assertTrue(model.isWinner());
     }
 
     @Test
     void boardShouldBeWonHorizontally() {
-        model.setToken(1,0); // X
-        model.setToken(0,0); // O
-        model.setToken(1,1); // X
-        model.setToken(0,1); // O
-        model.setToken(2,0); // X
-        model.setToken(0,2); // O
+        model.setToken(1, 0); // X
+        model.setToken(0, 0); // O
+        model.setToken(1, 1); // X
+        model.setToken(0, 1); // O
+        model.setToken(2, 0); // X
+        model.setToken(0, 2); // O
 
         assertTrue(model.isWinner());
     }
 
     @Test
     void boardShouldBeWonDiagonally() {
-        model.setToken(0,0); // X
-        model.setToken(0,1); // O
-        model.setToken(1,1); // X
-        model.setToken(0,2); // O
-        model.setToken(2,2); // X
+        model.setToken(0, 0); // X
+        model.setToken(0, 1); // O
+        model.setToken(1, 1); // X
+        model.setToken(0, 2); // O
+        model.setToken(2, 2); // X
 
         assertTrue(model.isWinner());
     }
 
     @Test
     void boardShouldBeWonDiagonally2() {
-        model.setToken(0,2); // X
-        model.setToken(0,1); // O
-        model.setToken(1,1); // X
-        model.setToken(0,0); // O
-        model.setToken(2,0); // X
+        model.setToken(0, 2); // X
+        model.setToken(0, 1); // O
+        model.setToken(1, 1); // X
+        model.setToken(0, 0); // O
+        model.setToken(2, 0); // X
 
         assertTrue(model.isWinner());
     }
 
     @Test
     void boardShouldBeFull() {
-        model.setToken(0,0); // X
-        model.setToken(0,1); // O
-        model.setToken(0,2); // X
-        model.setToken(1,1); // O
-        model.setToken(1,0); // X
-        model.setToken(2,0); // O
-        model.setToken(1,2); // X
-        model.setToken(2,2); // O
-        model.setToken(2,1); // X
+        model.setToken(0, 0); // X
+        model.setToken(0, 1); // O
+        model.setToken(0, 2); // X
+        model.setToken(1, 1); // O
+        model.setToken(1, 0); // X
+        model.setToken(2, 0); // O
+        model.setToken(1, 2); // X
+        model.setToken(2, 2); // O
+        model.setToken(2, 1); // X
 
         assertTrue(model.isFull());
     }
 
     @Test
     void boardShouldNotBeFull() {
-        model.setToken(0,0); // X
-        model.setToken(0,1); // O
+        model.setToken(0, 0); // X
+        model.setToken(0, 1); // O
 
         assertFalse(model.isFull());
     }
@@ -118,11 +118,11 @@ class ModelTest {
     void boardShouldResetAfterAWin() {
         String[][] board = {{"", "", ""}, {"", "", ""}, {"", "", ""}};
 
-        model.setToken(0,2); // X
-        model.setToken(0,1); // O
-        model.setToken(1,1); // X
-        model.setToken(0,0); // O
-        model.setToken(2,0); // X
+        model.setToken(0, 2); // X
+        model.setToken(0, 1); // O
+        model.setToken(1, 1); // X
+        model.setToken(0, 0); // O
+        model.setToken(2, 0); // X
 
         model.setState(State.PLAYING);
         assertArrayEquals(board, model.getBoard());
@@ -162,13 +162,29 @@ class ModelTest {
     @Test
     void p2PointsPropertyShouldReturnCorrectValueIfIsLocal() {
         model.setIsLocal(true);
+
         assertEquals("CPU (O): 0", model.p2PointsProperty().getValue());
     }
 
     @Test
     void p2PointsPropertyShouldReturnCorrectValueIfIsOnline() {
         model.setIsLocal(false);
+
         assertEquals("Player 2 (O): 0", model.p2PointsProperty().getValue());
+    }
+
+    @Test
+    void getCurrentPlayerShouldReturnUpdatedPlayer() {
+        model.setCurrentPlayer("O");
+
+        assertEquals("O", model.getCurrentPlayer());
+    }
+
+    @Test
+    void getIsLocalShouldReturnUpdatedState() {
+        model.setIsLocal(false);
+
+        assertFalse(model.getIsLocal());
     }
 
 }
