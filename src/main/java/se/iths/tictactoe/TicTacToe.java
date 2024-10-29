e22728d99f4fc69388a040146ce3fb2686b2cd20
voffie <viktorsoderborg@gmail.com>
2024-10-29 12:54:31 +0100

    Remove unused "this"

--- src/main/java/se/iths/tictactoe/TicTacToe.java
+++ src/main/java/se/iths/tictactoe/TicTacToe.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class TicTacToe extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("tictactoe-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("tictactoe-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Tic Tac Toe");
         stage.setScene(scene);
